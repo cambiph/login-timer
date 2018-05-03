16583855bdae03762365e0e7a07950e4cf57a391
Philippe Cambien <philippe.cambien@gmail.com>
2018-05-03 14:04:13 +0200

    Add babel support + catch timeouts

--- main.js
+++ main.js
@@ -6,44 +6,51 @@ const user = process.env.user || "delijn@inboxbear.com";
 const password = process.env.password || "DeLijn01";
 
 (async () => {
-    console.log('Using url: ' + url);
+    try {
+        const timeout = 60000;
+        console.log('Using url: ' + url);
+        console.log('Using timeout: ' + timeout + ' ms');
 
-    const browser = await puppeteer.launch({
-        headless: true,
-        timeout: 60000
-    });
+        const browser = await puppeteer.launch();
 
-    const page = await browser.newPage();
-    page.setViewport({
-        width: 1920,
-        height: 1080
-    });
+        const page = await browser.newPage();
+        page.setViewport({
+            width: 1920,
+            height: 1080
+        });
 
-    await page.setCookie({
-        'name': 'noodberichtenCookie',
-        'value': '%5B12%2004%202018%2012%3A57%3A29_9336%5D',
-        'domain': 'accept.delijn.be',
-        'expires': Date.now() / 1000 + 10,
-    });
-    
-    await page.goto(url);
+        await page.setCookie({
+            'name': 'noodberichtenCookie',
+            'value': '%5B12%2004%202018%2012%3A57%3A29_9336%5D',
+            'domain': 'accept.delijn.be',
+            'expires': Date.now() / 1000 + 10,
+        });
 
-    await page.click('#login-btn-header');
-    await page.waitForSelector('#user-login-email');
+        await page.goto(url, {timeout: timeout});
 
-    await page.type('#user-login-email', user);
-    await page.type('#user-login-password', password);
-    await page.click('#loginbutton');
+        await page.click('#login-btn-header');
+        await page.waitForSelector('#user-login-email');
 
-    let start = new Date()
-    await page.waitForSelector('button#userButton span.icon.icon-caret-down', {timeout: 60000});
+        await page.type('#user-login-email', user);
+        await page.type('#user-login-password', password);
+        await page.click('#loginbutton');
 
-    let stop = new Date()
-    let timing = (stop.getTime() - start.getTime()) / 1000;
+        let start = new Date();
 
-    await browser.close();
+        await page.waitForSelector('button#userButton span.icon.icon-caret-down', {
+            timeout: timeout
+        });
 
-    fs.appendFile('timing.txt', new Date().toISOString() + ' ' + timing + '\n' , (err) => {
-        if (err) throw err;
-    });
+        let stop = new Date();
+        let timing = (stop.getTime() - start.getTime()) / 1000;
+
+        await browser.close();
+
+        fs.appendFile('timing.txt', new Date().toISOString() + ' ' + timing + '\n', (err) => {
+            if (err) throw err;
+        });
+
+    } catch (err) {
+        console.log(err);
+    }
 })();
